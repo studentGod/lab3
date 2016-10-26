4184f981cbdb1ad9c3c668215b1177cb8e845179
Hao <zsh9519@gmail.com>
2016-10-26 17:57:16 -0600

    Finish all

--- src/PKG/main.java
+++ src/PKG/main.java
@@ -8,26 +8,16 @@ public class main {
 		// TODO Auto-generated method stub
 		
 		LinkedList li=new LinkedList();
+		int k=1;
 		
-		
-		
-		li.addNodeBack(4);
-		li.addNodeBack(3);
-		li.addNodeBack(1);
-		li.addNodeBefore(44);
-		li.addNodeBefore(11);
-		li.print();
-		li.deletNode(1);
-		li.print();
-		
-		/*int k=1;
-		
-		while(k==1||k==2||k==3||k==4)
+		while(k==1||k==2||k==3||k==4||k==5||k==6)
 		{
-			System.out.println("Press <1> for adding nodes");
+			System.out.println("Press <1> for adding the node before");
 			System.out.println("Press <2> for deleting nodes");
 			System.out.println("Press <3> for finding nodes");
-			System.out.println("Press <4> for printing nodes");
+			System.out.println("Press <4> for printing nodes from head");
+			System.out.println("Press <5> for adding the node after");
+			System.out.println("Press <6> for printing nodes from tail");
 			Scanner n= new Scanner(System.in);
 			k=n.nextInt();
 			switch(k)
@@ -35,7 +25,7 @@ public class main {
 				System.out.println("Enter the number you want to add: ");
 				Scanner n1= new Scanner(System.in);
 				int i=n1.nextInt();
-				li.addNode(i);
+				li.addNodeBefore(i);
 				break;
 			case 2:
 				System.out.println("Enter the number you want to delete: ");
@@ -60,10 +50,19 @@ public class main {
 			case 4:
 				li.print();
 				break;
+			case 5:
+				System.out.println("Enter the number you want to add: ");
+				Scanner n4= new Scanner(System.in);
+				int p=n4.nextInt();
+				li.addNodeBack(p);
+				break;
+			case 6:
+				li.printFromTail();
+				break;
 			default:
 				System.out.println("Exit the program");
 			}
-	}*/
+	}
 		
 
 	}
